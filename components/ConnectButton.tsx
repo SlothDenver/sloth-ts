09fe3a229e9bf89deafa56ce89902f6d3296eaed
indiaprince <hyeonsuhan@gmail.com>
2023-03-04 15:10:11 +0900

    Connection button Fixed

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -1,5 +1,5 @@
 import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
-import { injected, walletconnect } from "@/connectors/connectors";
+import { injected , walletconnect} from "@/connectors/connectors";
 
 //import { getErrorMessage }      TODO  : ERROR CODES
 //import { switchChains }         TODO  : SWITCHING WALLET
@@ -20,32 +20,63 @@ import { useEffect, useState } from "react";
 import { useLoading } from "@/hooks/useLoading";
 import { useIsConnected } from "@/hooks/useIsConnected";
 import { useFuel } from "@/hooks/useFuel";
+import { getErrorMessage } from "@/helper/getErrorMessage";
 
 function ConnectButton() {
   //
   //const errorMessage = errorConnect || errorDisconnect || notDetected; // error code for fuel
   //const errorMessage = getErrorMessage(error)                           error code for metamask
   const { Text } = Typography;
-  const [fuel, notDetected] = useFuel();
-  const [isConnected] = useIsConnected();
-  const [isFuel, setIsFuel] = useState(false);
-  const [handleDisconnect, isDisconnecting, errorDisconnect] = useLoading(
+  const [fuel, FuelnotDetected] = useFuel();
+  const [isFuelConnected] = useIsConnected();
+
+  const [handleFuelDisconnect, isFuelDisconnecting, errorFuelDisconnect] = useLoading(
     async () => {
       await fuel?.disconnect();
-    }
-  );
-  const [handleConnect, isConnecting, errorConnect] = useLoading(async () => {
+  });
+  const [handleFuelConnect, isFuelConnecting, errorFuelConnect] = useLoading(
+    async () => {
     await fuel?.connect();
   });
-  const [accounts, setAccounts] = useState<string[]>([]);
+  const [Fuelaccounts, setFuelAccounts] = useState<string[]>([]);
 
-  const [handleAccounts, errorAccounts] = useLoading(async () => {
-    const accounts = await fuel.accounts();
-    setAccounts(accounts);
+  const [handleFuelAccounts, errorFuelAccounts] = useLoading(
+    async () => {
+      const accounts = await fuel.accounts();
+      setFuelAccounts(accounts);
   });
 
-  const handleAccountsEvent = (accounts: string[]) => {
-    setAccounts(accounts);
+  const handleFuelAccountsEvent = (accounts: string[]) => {
+    setFuelAccounts(accounts);
+  };
+
+
+
+  useEffect(() => {
+    fuel?.on("accounts", handleFuelAccountsEvent);
+    return () => {
+      fuel?.off("accounts", handleFuelAccountsEvent);
+    };
+  }, [fuel]);
+  
+  useEffect(() => {
+    if (isFuelConnected) handleFuelAccounts();
+  }, [isFuelConnected]);
+
+  // For MetaMask
+  const { activate, active, account, error, deactivate} = useWeb3React();
+
+
+  const FuelerrorMessage = errorFuelConnect || errorFuelDisconnect || FuelnotDetected;
+  const EtherrorMessage = getErrorMessage(error)
+
+  const handleEthDisConnect = () => {
+    deactivate();
+  }
+  const handleEthConnect = async () => {
+    await activate(injected, (error) => {
+      getErrorMessage(error)
+    });
   };
 
   const items: MenuProps["items"] = [
@@ -54,10 +85,7 @@ function ConnectButton() {
       label: (
         <Text
           className="flex text-white cursor-pointer items-center font-medium rounded-lg hover:bg-black transition"
-          onClick={() => {
-            handleConnect();
-            setIsFuel(true);
-          }}
+          onClick={handleFuelConnect}
         >
           <Image
             src="/images/connectors/fuel-logo.svg"
@@ -75,10 +103,7 @@ function ConnectButton() {
       label: (
         <Text
           className="flex text-white cursor-pointer items-center  font-medium rounded-lg hover:bg-black transition"
-          onClick={async () => {
-            await activate(injected);
-            setIsFuel(false);
-          }}
+          onClick={handleEthConnect}
         >
           <Image
             src="/images/connectors/metamask-fox.svg"
@@ -98,7 +123,6 @@ function ConnectButton() {
           className="flex text-white cursor-pointer items-center  font-medium rounded-lg hover:bg-black transition"
           onClick={async () => {
             await activate(walletconnect);
-            setIsFuel(false);
           }}
         >
           <Image
@@ -114,38 +138,39 @@ function ConnectButton() {
     },
   ];
 
-  useEffect(() => {
-    fuel?.on("accounts", handleAccountsEvent);
-    return () => {
-      fuel?.off("accounts", handleAccountsEvent);
-    };
-  }, [fuel]);
-  useEffect(() => {
-    if (isConnected) handleAccounts();
-  }, [isConnected]);
-
-  // For MetaMask
-  const { activate, active, account, error } = useWeb3React();
-  const accountFormatted = isConnected
-    ? accounts[0].substring(0, 6) + "..." + accounts[0].substring(26, 32)
-    : account?.substring(0, 6) + "..." + account?.substring(26, 32);
-
-  if (isConnected) {
+  if(active) {
     return (
-      // <div className="flex items-center font-semibold 2xl:text-xl gap-2 text-dark-800 tracking-2%">
-      //   {
-      //     <div className="tracking-2% md:text-sm 2xl:text-base">
-      //       <div className="flex items-center gap-3">
-      //         <span className="uppercase text-dark-800">Wallet</span>
-      //         <span className="w-3 h-3 rounded-full bg-[#24DB4D]"></span>
-      //       </div>
-      //       <p className="text-dark-100">{accountFormatted}</p>
-      //     </div>
-      //   }
-      // </div>
-      <div className="flex md:px-2 2xl:px-4">
-        <Select className="w-36 text-white ">
-          <Select.Option value="1">
+      <div className="flex md:px-2 2xl:px-4 items-center">
+          <Image
+            src="/images/connectors/metamask-fox.svg"
+            width={24}
+            priority
+            height={24}
+            alt="MetaMask"
+          />
+          <span className="text-white">MetaMask</span>
+        <Select 
+          className="w-36 mx-2 text-white"
+          defaultValue = {0}
+        >
+          {
+            <Select.Option value={0} key = {0}>
+          <span className="text-white">{account}</span>
+          </Select.Option>
+          }
+        </Select>
+        
+      <Button 
+        className="bg-black backdrop-blur text-white"
+        onClick={handleEthDisConnect}>
+        <LogoutOutlined />
+      </Button>
+    </div>
+    )
+  }
+  if (isFuelConnected) {
+    return (
+      <div className="flex md:px-2 2xl:px-4 items-center">
             <Image
               src="/images/connectors/fuel-logo.svg"
               width={24}
@@ -154,36 +179,33 @@ function ConnectButton() {
               alt="FuelConnect"
             />
             <span className="text-white">Fuel</span>
-          </Select.Option>
-          <Select.Option value="2">
-            <Image
-              src="/images/connectors/metamask-fox.svg"
-              width={24}
-              priority
-              height={24}
-              alt="MetaMask"
-            />
-            <span className="text-white">MetaMask</span>
-          </Select.Option>
-        </Select>
-        <Select className="w-36 mx-2 text-white ">
-          <Select.Option value="1">
-            <span className="text-white">{accountFormatted}</span>
-          </Select.Option>
-          <Select.Option value="2">
-            <span className="text-white">{accountFormatted}</span>
-          </Select.Option>
-        </Select>
-        <Button className="bg-black backdrop-blur text-white">
+          <Select 
+            className="w-36 mx-2 text-white"
+            defaultValue = {0}
+          >
+            {
+            Fuelaccounts.map((account, i) => (
+              <Select.Option value={i} key = {i}>
+            <span className="text-white">{account}</span>
+            </Select.Option>
+            ))
+            }
+          </Select>
+          
+        <Button 
+          className="bg-black backdrop-blur text-white"
+          onClick={handleFuelDisconnect}>
           <LogoutOutlined />
         </Button>
       </div>
     );
   }
+
+
   return (
     <div className="md:px-2 2xl:px-4">
       <Dropdown menu={{ items }} trigger={["click"]}>
-        <Button className="bg-[#F1FF0099] text-black border-none h-10 text-base font-bold">
+        <Button className="bg-[#F1FF0099] text-black border-none h-10 text-base font-bold" >
           Connect Wallet
         </Button>
       </Dropdown>
